642e422a4ce4e93e83b74e8859466709b00ed388
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-09-26 14:57:51 -0500

    creacion del proyecto

---
+++ src/main/java/pe/nico/cibertec/ExamenDawiNicolasSegoviaApplication.java
@@ -0,0 +1,13 @@
+package pe.nico.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenDawiNicolasSegoviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenDawiNicolasSegoviaApplication.class, args);
+	}
+
+}
