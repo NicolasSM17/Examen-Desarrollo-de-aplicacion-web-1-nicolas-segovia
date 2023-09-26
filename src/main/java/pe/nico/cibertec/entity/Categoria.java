33100483d0687f49ddff7a4284be4aafcf2bd8a1
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-09-26 15:11:31 -0500

    agregar atributos y metodos a las clases entidad cliente y categoria

--- src/main/java/pe/nico/cibertec/entity/Categoria.java
+++ src/main/java/pe/nico/cibertec/entity/Categoria.java
@@ -1,5 +1,17 @@
 package pe.nico.cibertec.entity;
 
-public class Categoria {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "cliente")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Categoria {
+	private int id;
+	private String nombre;
 }
--- src/main/java/pe/nico/cibertec/entity/Cliente.java
+++ src/main/java/pe/nico/cibertec/entity/Cliente.java
@@ -1,5 +1,23 @@
 package pe.nico.cibertec.entity;
 
-public class Cliente {
+import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cliente")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Cliente {
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private Date fechaNacimiento;
+	private String dni;
 }
